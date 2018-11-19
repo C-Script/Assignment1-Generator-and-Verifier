55977555bc81f931e38dca591184a1dd51ce8d38
Abdo <abdopoetry@gmail.com>
2018-11-19 17:27:03 +0200

    Made sure the generator file properly reads the message and the generator function from the input file

--- generator.py
+++ generator.py
@@ -1,1 +1,20 @@
 # The generator / sender function
+
+
+# 1-Taking the Input
+
+print("Generator")
+F = open("generator_input.txt", "r")
+input = F.readlines()
+
+message = input[0].rstrip()
+generator = input[1]
+
+print("Message is "+message)
+print("Generator is "+generator)
+
+
+# Output
+file = open("generator_output.txt", "w")
+
+file.write("I am the generator output")
