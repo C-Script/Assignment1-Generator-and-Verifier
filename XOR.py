e638aacc82a5912a68a245eeee68d886a721074e
Mohamed-Farag <33036643+Mohamed-Farag@users.noreply.github.com>
2018-11-20 00:33:39 +0200

    Update XOR.py

--- XOR.py
+++ XOR.py
@@ -1,23 +1,19 @@
 ### The function that is called in the generator and the verifier files ###
 
-
-######################### Helper Functions #########################
-
-def mainXOR(s1, s2):
-    result = ""
+def XOR ( s1 , s2 ):
+    result= ""
     if(len(s1) != len(s2)):
         return 0
     else:
         for i in range(len(s1)):
-            if((s1[i] == "1" and s2[i] == "1") or (s1[i] == "0" and s2[i] == "0")):
-                result += "0"
+            if( (s1[i] == "1" and s2[i] == "1" ) or (s1[i] == "0" and s2[i] == "0") ):
+                result+="0"
             else:
-                result += "1"
+                result+="1"
 
     return result
 
-
-def elimnating_left_zeros(s):
+def elimnating_Left_zeros(s):
     result = s
     for i in range(len(s)):
         if(s[i] == "0"):
@@ -27,34 +23,49 @@ def elimnating_left_zeros(s):
 
     return ""
 
-
-######################### Primary Function #########################
-
-def XOR(message, G):
-
-    i = 0
-    message_slice = ""
-    lengthOfMessage_slice = 0
-
+def Adding_Left_zeros(s,len_G):
     while(1):
-        if(((i+len(G)) - lengthOfMessage_slice) <= len(message)):
-            message_slice = message_slice + \
-                message[i:((i+len(G)) - lengthOfMessage_slice)]
-        else:
-            message_slice = message_slice + message[i:]
-        i = ((i + len(G)) - lengthOfMessage_slice)
 
-        if(len(message_slice) < len(G)):
-            break
-        # message after XOR
-        message_slice = mainXOR(message_slice, G)
+         if(len(s) < len_G - 1):
+             s = "0" + s
+         elif (len(s) == len_G - 1):
+             return s
 
-        # message after elimnating left zeros
-        message_slice = elimnating_left_zeros(message_slice)
 
-        lengthOfMessage_slice = len(elimnating_left_zeros(message_slice))
 
-    while(len(message_slice) < len(G)-1):
-        message_slice = "0"+message_slice
 
-    return message_slice
+message = "101110000"      # str                  #7ot ya abdo al message bta3tk mkan de
+G = "1001"                     # lentgh=4                #7ot ya abdo al G bta3tk mkan de
+i = 0
+message_slice = ""
+lengthOfMessage_slice = 0
+flag = 0
+while(1):
+    if(((i+len(G)) - lengthOfMessage_slice) < len(message)):
+        message_slice = message_slice + message[i:((i+len(G)) - lengthOfMessage_slice)]
+    else:
+        message_slice = message_slice + message[i:]
+        flag = 1
+    i = ((i + len(G)) - lengthOfMessage_slice)
+    #print(message_slice)
+    if(len(message_slice) < len(G)):
+        break
+    message_slice = XOR(message_slice,G)                      # message after XOR
+    #print(message_slice)
+    message_slice = elimnating_Left_zeros(message_slice)      # message after elimnating left zeros
+    #print(message_slice,"\n")
+    lengthOfMessage_slice = len(elimnating_Left_zeros(message_slice))
+    if (flag == 1):
+        break
+
+
+message_slice = Adding_Left_zeros(message_slice,len(G))
+#print("#######" + message_slice + "#########")
+
+file = open("generator_output.txt", "w")
+file.write(message_slice)
+
+
+
+
+
