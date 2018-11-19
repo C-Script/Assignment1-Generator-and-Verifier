72ee90d185958affad08720d947afe70f33972a3
Abdo <abdopoetry@gmail.com>
2018-11-20 00:28:14 +0200

    centralized the app into main.js

--- generator.py
+++ generator.py
@@ -1,35 +1,18 @@
 ### The generator / sender function ###
 from XOR import XOR
 
-# 1-Taking the Input
 
-print("Generator")
-F = open("generator_input.txt", "r")
+def generator(message, g):
 
-input = F.readlines()
+    # 1-Adding necessary zeros to the message
+    generatorLength = len(g)
+    appendedZerosLength = generatorLength-1
 
-message = input[0].rstrip()
-generator = input[1]
+    for x in range(appendedZerosLength):
+        message = message+"0"
 
+    # 2-Doing XOR between message and generator
+    r = XOR(message, g)
 
-# 2-Adding necessary zeros to the message
-
-generatorLength = len(generator)
-appendedZerosLength = generatorLength-1
-
-for x in range(appendedZerosLength):
-    message = message+"0"
-
-print("Message is "+message)
-print("Generator is "+generator)
-
-
-# 3-Doing XOR between message and generator
-result = XOR(message, generator)
-print("Result is "+result)
-
-
-# 4-Printing the output
-file = open("generator_output.txt", "w")
-
-file.write("I am the generator output")
+    transmittedMessage = message + r
+    return transmittedMessage
---
+++ main.py
@@ -0,0 +1,28 @@
+
+from generator import generator
+
+# 1-Taking the Input
+
+print("Calling Generator")
+inputFile = open("generator_input.txt", "r")
+
+input = inputFile.readlines()
+
+message = input[0].rstrip()
+g = input[1]
+
+
+# 2- Getting transmitted message from generator
+transmitted_message = generator(message, g)
+
+# 3- Altering
+## if (alter option is chosen for a bit) alter this bit ##
+
+# 4- Verifying the message through verifier
+print("Calling Verifier")
+
+
+# 5- Printing the output
+outputFile = open("generator_output.txt", "w")
+
+outputFile.write("transmitted message: " + transmitted_message)
