80cd6ac5da347a7f6067c6344cebe1b238357954
Mohamed-Farag <33036643+Mohamed-Farag@users.noreply.github.com>
2018-11-20 12:18:41 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -7,12 +7,12 @@ def generator(message, g):
     # 1-Adding necessary zeros to the message
     generatorLength = len(g)
     appendedZerosLength = generatorLength-1
-
+    original_message = message
     for x in range(appendedZerosLength):
         message = message+"0"
 
     # 2-Doing XOR between message and generator
     r = XOR(message, g)
 
-    transmittedMessage = message + r
+    transmittedMessage = original_message + r
     return transmittedMessage
